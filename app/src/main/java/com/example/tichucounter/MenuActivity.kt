3118b7ffdc5a48a36de6353356a1ab6e30b01216
georgekal2798 <38210685+georgekal2798@users.noreply.github.com>
2022-04-21 16:39:18 +0300

    New game intent
    - Added intent to start a new game

--- app/src/main/java/com/example/tichucounter/MenuActivity.kt
+++ app/src/main/java/com/example/tichucounter/MenuActivity.kt
@@ -1,11 +1,20 @@
 package com.example.tichucounter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+    }
+
+    // Triggered by the onClick attribute of newGameButton
+    fun newGame(view: View) {
+        val intent = Intent(this, ScoreActivity::class.java)
+        startActivity(intent)
     }
 }
